90f0c49f3552c7703d52054c99ca19cfe05753b4
Jong Cheon Park <simpleego@gmail.com>
2022-04-19 08:18:59 +0900

    Add files via upload

---
+++ sample01/src/sample01/Sample.java
@@ -0,0 +1,15 @@
+package sample01;
+
+public class Sample {
+
+	public static void main(String[] args) {
+		
+		System.out.println("¾È³ç Java1");
+		System.out.println("¾È³ç Java2");
+		System.out.println("¾È³ç Java4");
+		System.out.println("¾È³ç Java3");
+		System.out.println("¾È³ç Java5");
+
+	}
+
+}
