db867cfef719e844405590e5d6145a49aedcd02c
Jong Cheon Park <simpleego@gmail.com>
2022-04-19 08:19:19 +0900

    Add files via upload

---
+++ sample02/src/com/simple/ex/Hello.java
@@ -0,0 +1,14 @@
+package com.simple.ex;
+
+public class Hello {
+
+	public static void main(String[] args) {
+		
+		System.out.println("-------------");
+		System.out.println("Hello Java1");
+		System.out.println("Hello\tJava");
+		System.out.println("-------------");
+
+	}
+
+}
---
+++ sample02/src/sample02/Sample.java
@@ -0,0 +1,8 @@
+package sample02;
+
+public class Sample {
+
+	public static void main(String[] args) {
+		System.out.println("¾È³ç ÀÚ¹Ù!!");
+	}
+}
